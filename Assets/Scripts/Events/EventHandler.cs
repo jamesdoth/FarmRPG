f8e8b9a56864d3dcb74e4c1530bc8537fd16b010
jamesdoth <jamesdoth@gmail.com>
2023-08-30 09:34:08 -0700

    event and event handler

---
+++ Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,32 @@
+public delegate void MovementDelegate(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying, 
+    ToolEffect toolEffect, 
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown, 
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown, 
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown, 
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown, 
+    bool idleRight, bool idleLeft, bool idleUp, bool idleDown);
+
+public static class EventHandler
+{
+    public static event MovementDelegate MovementEvent;
+
+    public static void CallMovementEvent(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool idleRight, bool idleLeft, bool idleUp, bool idleDown)
+    {
+        if (MovementEvent != null)
+        {
+            MovementEvent(inputX, inputY, isWalking, isRunning, isIdle, isCarrying,
+                toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                idleRight, idleLeft, idleUp, idleDown);
+        }
+    }
+}
